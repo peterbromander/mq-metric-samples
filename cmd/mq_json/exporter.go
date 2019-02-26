7be0ef4135b501746d637bda3293ad44820c227c
Peter Bromander <peter.bromander@dogfish.se>
2019-02-26 13:17:16 +0100

    added http post

--- cmd/mq_json/exporter.go
+++ cmd/mq_json/exporter.go
@@ -243,10 +243,32 @@ func Collect() error {
 		b, _ := json.MarshalIndent(j, "", "  ")
 		fmt.Printf("%s\n", b)
 
+		postToUrl("http://10.40.10.72:5059",b)
+
 	}
 
 	return err
 
+} 
+func postToUrl(url String, jsonStr String) {
+   // url := "http://restapi3.apiary.io/notes"
+    fmt.Println("URL:>", url)
+
+   // var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
 }
 
 func fixup(s1 string) string {
